54da4a046915ff4c9f88ca9ae64e5cb50a1404e2
Said <saidziani.pro@gmail.com>
2018-02-11 20:08:45 +0100

    adjust label places.

--- gui/Application.py
+++ gui/Application.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-import SearchSpaceBool, SearchSpaceVec, EvaluationSpace
+import PosWorkSpace
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -15,14 +15,14 @@ class Ui_Form(object):
         self.hideWindow = Form.hide
 
         self.label = QtWidgets.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(150, 330, 150, 51))
+        self.label.setGeometry(QtCore.QRect(200, 330, 150, 51))
         font = QtGui.QFont()
         font.setFamily("Lato")
         font.setPointSize(15)
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(Form)
-        self.label_2.setGeometry(QtCore.QRect(530, 330, 150, 51))
+        self.label_2.setGeometry(QtCore.QRect(450, 330, 150, 51))
         font = QtGui.QFont()
         font.setFamily("Lato")
         font.setPointSize(15)
@@ -30,22 +30,9 @@ class Ui_Form(object):
         self.label_2.setObjectName("label_2")
 
         self.label_3 = QtWidgets.QLabel(Form)
-        self.label_3.setGeometry(QtCore.QRect(275, 330, 21, 51))
+        self.label_3.setGeometry(QtCore.QRect(380, 330, 21, 51))
 
-        self.label_7 = QtWidgets.QLabel(Form)
-        self.label_7.setGeometry(QtCore.QRect(465, 330, 21, 51))
-
-        self.label_6 = QtWidgets.QLabel(Form)
-        self.label_6.setGeometry(QtCore.QRect(340, 330, 100, 51))
-        self.label_6.setFont(font)
-        self.label_6.setObjectName("label_6")
-        self.label_6.setStyleSheet("font-weight:600")
-        self.label_6.mousePressEvent = self.openEvaluation
-
-        # self.label_3.setFont(font)
         self.label_3.setObjectName("label_3")
-        # self.label_7.setFont(font)
-        self.label_7.setObjectName("label_7")
 
         font = QtGui.QFont()
         font.setFamily("Lato")
@@ -60,8 +47,8 @@ class Ui_Form(object):
         self.label_4.setPixmap(pixmap)
         self.label_4.setGeometry(QtCore.QRect(313, 100, 220, 200))
 
-        self.label.mousePressEvent = self.openBoolean
-        self.label_2.mousePressEvent = self.openVectorial
+        self.label.mousePressEvent = self.openPos
+        # self.label_2.mousePressEvent = self.openSum
 
         self.label_3.setStyleSheet("font-weight:100")
         self.label_2.setStyleSheet("font-weight:600")
@@ -83,34 +70,25 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "MonChef."))
-        self.label.setText(_translate("Form", "Recherche\nBooléenne"))
-        self.label_2.setText(_translate("Form", "Recherche\nVectorielle"))
-        self.label_6.setText(_translate("Form", "Évaluation"))
+        Form.setWindowTitle(_translate("Form", "Mou3ini."))
+        self.label.setText(_translate("Form", "POS Tagging"))
+        self.label_2.setText(_translate("Form", "Summarizaion"))
         self.label_3.setText(_translate("Form", " |"))
-        self.label_7.setText(_translate("Form", " |"))
-
         self.label_5.setText(_translate("Form", ""))
 
 
-    def openBoolean(self, event):
+    def openPos(self, event):
         self.window = QtWidgets.QMainWindow()
-        self.ui = SearchSpaceBool.SearchSpaceBool()
+        self.ui = PosWorkSpace.PosWorkSpace()
         self.ui.setupUi(self.window)
         self.window.show()
 
 
-    def openVectorial(self, event):
-        self.window = QtWidgets.QWidget()
-        self.ui = SearchSpaceVec.SearchSpaceVec()
-        self.ui.setupUi(self.window)
-        self.window.show()
-
-    def openEvaluation(self, event):
-        self.window = QtWidgets.QWidget()
-        self.ui = EvaluationSpace.EvaluationSpace()
-        self.ui.setupUi(self.window)
-        self.window.show()
+    # def openSum(self, event):
+    #     self.window = QtWidgets.QWidget()
+    #     self.ui = SearchSpaceVec.SearchSpaceVec()
+    #     self.ui.setupUi(self.window)
+    #     self.window.show()
 
 
     def retour(self, event):
